1abbb66e18610ba255bdf0961817a6b6a89e76cf
kenfack2 <herold.kenfack@facsciences>
2024-10-08 17:20:15 +0100

    firstC

---
+++ code_cour/fabrique/ProduitFactory2.java
@@ -0,0 +1,5 @@
+public class ProduitFactory2 extends ProduitFactory{
+    protected ProduitA createProduitA(){
+        return new ProduitA2();
+    }
+}
---
+++ code_cour/fabrique/ProduitFactory3.java
@@ -0,0 +1,5 @@
+public class ProduitFactory3 extends ProduitFactory{
+    protected ProduitA createProduitA(){
+        return new ProduitA3();
+    }
+}
